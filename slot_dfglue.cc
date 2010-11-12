34a058f1e1b08526a64df9d7e9b091ba57cab7d0
Mikko Juola <mikjuo@gmail.com>
2010-11-12 22:35:49 +0200

    DF 0.31.17 SDL support on Windows.

--- slot_dfglue.cc
+++ slot_dfglue.cc
@@ -775,6 +775,13 @@ bool DFGlue::detectDFVersion()
 
     switch(csum)
     {
+    case 0xbeed6089: /* DF 0.31.17 (SDL) */
+    af.pushAddress(0x000060C0, "dfterm_injection_glue.dll");
+    sz.pushAddress(0x0071107C, utf8_image_base_name);
+    data_format = PackedVarying;
+    df_version = 3117;
+    LOG(Note, "Dwarf Fortress executable checksum calculated to " << (void*) csum << " (DF 0.31.17 SDL version)");
+    break;
     case 0x92589853: /* DF 0.31.16 (SDL) */
     af.pushAddress(0x000060C0, "dfterm_injection_glue.dll");
     sz.pushAddress(0x013DE914, utf8_image_base_name);
